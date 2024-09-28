cf1d54182e7e47b239481169bf62d979a2f33205
ZeroByter <romgamesrom@gmail.com>
2024-09-28 23:15:57 +0300

    some more changes

--- src/components/shared/buttonLink.tsx
+++ src/components/shared/buttonLink.tsx
@@ -3,12 +3,17 @@ import css from "./buttonLink.module.scss";
 
 type Props = {
   href: string;
+  blankTarget?: boolean;
   children: ReactNode;
 };
 
-const ButtonLink: FC<Props> = ({ href, children }) => {
+const ButtonLink: FC<Props> = ({ href, children, blankTarget }) => {
+  const blankTargetProps = blankTarget
+    ? { target: "_blank", rel: "noreferrer" }
+    : {};
+
   return (
-    <a className={css.link} href={href}>
+    <a className={css.link} href={href} {...blankTargetProps}>
       <button className={css.button}>{children}</button>
     </a>
   );
--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -1,5 +1,6 @@
 import MainLogo from "../../components/mainLogo";
 import Sections from "../../components/section";
+import ButtonLink from "../../components/shared/buttonLink";
 import css from "./page.module.scss";
 
 export default function Home() {
@@ -7,33 +8,34 @@ export default function Home() {
     <div className={css.container}>
       <MainLogo />
       <div className={css.quote}>{'"I love making things"'}</div>
-      <div className={css.links}>
-        <a href="https://github.com/ZeroByter" target="_blank" rel="noreferrer">
-          GitHub
-        </a>
-        <a
-          href="https://store.steampowered.com/developer/zerobyter"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Steam
-        </a>
-      </div>
-      <div className={css.skills}>
-        <div className={css.header}>Skills:</div>
-        <ul>
-          <li>Web Development:</li>
+      <div className={css.headerInfo}>
+        <div className={css.skills}>
+          <div className={css.header}>Skills:</div>
           <ul>
-            <li>60% Frontend - 40% Backend</li>
+            <li>Web Development:</li>
+            <ul>
+              <li>60% Frontend - 40% Backend</li>
+            </ul>
+            <li>NodeJS + TypeScript</li>
+            <li>React</li>
+            <ul>
+              <li>Redux</li>
+              <li>GraphQL</li>
+            </ul>
+            <li>C#</li>
           </ul>
-          <li>NodeJS + TypeScript</li>
-          <li>React</li>
-          <ul>
-            <li>Redux</li>
-            <li>GraphQL</li>
-          </ul>
-          <li>C#</li>
-        </ul>
+        </div>
+        <div className={css.links}>
+          <ButtonLink blankTarget={true} href="https://github.com/ZeroByter">
+            GitHub
+          </ButtonLink>
+          <ButtonLink
+            blankTarget={true}
+            href="https://store.steampowered.com/developer/zerobyter"
+          >
+            Steam
+          </ButtonLink>
+        </div>
       </div>
       <Sections />
     </div>
