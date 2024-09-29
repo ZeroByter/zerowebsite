162ff28ed7cf48ebe3dd4b0550b540fac62f63cb
ZeroByter <romgamesrom@gmail.com>
2024-09-29 10:57:11 +0300

    added some stuff

--- src/siteData.ts
+++ src/siteData.ts
@@ -3,6 +3,7 @@ import paintOnlineIndexLogo from "./imgs/paintonline/indexLogo.png";
 import galacticLanderIndexLogo from "./imgs/galacticlander/indexLogo.png";
 import tenDaysToWarIndexLogo from "./imgs/tendaystowar/indexLogo.png";
 import pixelPaintersIndexLogo from "./imgs/pixelpainters/indexLogo.png";
+import heatmapIndexLogo from "./imgs/heatmap/indexLogo.png";
 
 const siteData: SiteDataType = {
   sections: [
@@ -31,6 +32,22 @@ const siteData: SiteDataType = {
       ],
     },
     {
+      title: "HTML5 Canvas Projects",
+      skills: ["JavaScript", "HTML5 Canvas"],
+      projects: [
+        {
+          title: "Easy Snooker",
+          image: "https://www.placeholder.co/200",
+          mainLink: "https://zerobyter.itch.io/easy-snooker"
+        },
+        {
+          title: "Interactive Heatmap",
+          image: heatmapIndexLogo,
+          mainLink: "/game/heatmap"
+        }
+      ],
+    },
+    {
       title: "Website Projects",
       skills: ["NodeJS", "TypeScript", "React", "SASS"],
       projects: [
@@ -41,11 +58,6 @@ const siteData: SiteDataType = {
         },
       ],
     },
-    // {
-    //   title: "HTML5 Canvas Projects",
-    //   skills: ["JavaScript", "HTML5 Canvas"],
-    //   projects: [],
-    // },
   ],
 };
 
